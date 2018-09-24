195fd9b759e3b0f88eedb3556d18b6e30a0e593d
pichillilorenzo <pichillilorenzo@gmail.com>
2018-09-24 02:02:12 +0200

    v0.1.0

--- example/flutter_plugin/example/README.md
+++
@@ -1,8 +0,0 @@
-# flutter_plugin_example
-
-Demonstrates how to use the flutter_plugin plugin.
-
-## Getting Started
-
-For help getting started with Flutter, view our online
-[documentation](https://flutter.io/).
--- example/flutter_plugin/ios/Classes/FlutterPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "FlutterPlugin.h"
-#import <flutter_plugin/flutter_plugin-Swift.h>
-
-@implementation FlutterPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterPlugin registerWithRegistrar:registrar];
-}
-@end

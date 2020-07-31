699c731a4036137d298d7315d1fd1231875c8fd5
edmund <edmundtay96@gmail.com>
2020-07-31 20:35:27 +0800

    Fix: added compatibility header

--- ios/Classes/InAppWebViewFlutterPlugin.m
+++ ios/Classes/InAppWebViewFlutterPlugin.m
@@ -16,7 +16,14 @@
  */
 
 #import "InAppWebViewFlutterPlugin.h"
+#if __has_include(<flutter_inappwebview/flutter_inappwebview-Swift.h>)
 #import <flutter_inappwebview/flutter_inappwebview-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "flutter_inappwebview-Swift.h"
+#endif
 
 @implementation InAppWebViewFlutterPlugin : NSObject
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {

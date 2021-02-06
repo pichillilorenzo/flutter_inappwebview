e7de6d34194b831bece25fc0411460a4bb2cecb3
Lorenzo Pichilli <pichillilorenzo@gmail.com>
2021-02-06 02:31:24 +0100

    removed unused imports InAppWebView.java

--- android/src/main/java/com/pichillilorenzo/flutter_inappwebview/InAppWebView/InAppWebView.java
+++ android/src/main/java/com/pichillilorenzo/flutter_inappwebview/InAppWebView/InAppWebView.java
@@ -16,7 +16,6 @@ import android.os.Message;
 import android.print.PrintAttributes;
 import android.print.PrintDocumentAdapter;
 import android.print.PrintManager;
-import android.util.ArraySet;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.ActionMode;

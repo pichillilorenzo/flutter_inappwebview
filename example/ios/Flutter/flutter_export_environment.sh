8e06d1d1b4477fb9a8eb88f5cdf4b338459e613f
Lorenzo Pichilli <pichillilorenzo@gmail.com>
2022-05-05 21:23:00 +0200

    fix deprecated reference in doc

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/lorenzopichilli/fvm/versions/2.10.4"
-export "FLUTTER_APPLICATION_PATH=/Users/lorenzopichilli/Desktop/flutter_inappwebview/example"
+export "FLUTTER_APPLICATION_PATH=/Users/lorenzopichilli/flutter_inappwebview_v5/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"

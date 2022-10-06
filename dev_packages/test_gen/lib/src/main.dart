33bea2527097e1782d0992e8aa33d5c9a3f7b42d
Lorenzo Pichilli <pichillilorenzo@gmail.com>
2022-10-06 16:51:32 +0200

    updated dev_packages

--- dev_packages/test_gen/lib/src/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:test_gen/src/types/main.dart';
-import 'package:test_gen/src/types/test_enum.dart';
-
-void main() {
-  TestClass test = TestClass(test1: "test1", test2: TestClass2(test1: ""), actionModeMenuItem: ActionModeMenuItem.MENU_ITEM_NONE);
-}
--- dev_packages/test_gen/lib/src/types/main.dart
+++
@@ -1,2 +0,0 @@
-export 'test_class.dart' show TestClass;
-export '../test/test_class_2.dart' show TestClass2;

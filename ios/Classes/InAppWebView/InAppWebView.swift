81528b85b1612b6f488cf70804408a3a579d77e1
Bogdan Popa <ice.bogdan@gmail.com>
2024-11-05 18:39:30 +0200

    fix

--- ios/Classes/InAppWebView/InAppWebView.swift
+++ ios/Classes/InAppWebView/InAppWebView.swift
@@ -1223,7 +1223,7 @@ public class InAppWebView: WKWebView, UIScrollViewDelegate, WKUIDelegate, WKNavi
         }
     }
     
-    public override func evaluateJavaScript(_ javaScriptString: String, completionHandler: ((Any?, Error?) -> Void)? = nil) {
+    public func evaluateJavaScript(_ javaScriptString: String, completionHandler: ((Any?, Error?) -> Void)? = nil) {
         if let applePayAPIEnabled = options?.applePayAPIEnabled, applePayAPIEnabled {
             if let completionHandler = completionHandler {
                 completionHandler(nil, nil)

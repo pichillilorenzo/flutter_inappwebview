fc75a0ad84bfa80b89ef477edf79b240c4eace9b
Ryan Feline <ryan.feline@trint.com>
2024-11-08 11:36:31 +0000

    Wrap show and hide into enable and disable call.

--- flutter_inappwebview_android/lib/src/in_app_webview/in_app_webview_controller.dart
+++ flutter_inappwebview_android/lib/src/in_app_webview/in_app_webview_controller.dart
@@ -2261,25 +2261,17 @@ class AndroidInAppWebViewController extends PlatformInAppWebViewController
   @override
   Future<void> disableInputMethod() async {
     Map<String, dynamic> args = <String, dynamic>{};
-    return await channel?.invokeMethod('disableInputMethod', args);
+    return channel
+        ?.invokeMethod('disableInputMethod', args)
+        .then((_) => channel?.invokeMethod('hideInputMethod', args));
   }
 
   @override
   Future<void> enableInputMethod() async {
     Map<String, dynamic> args = <String, dynamic>{};
-    return await channel?.invokeMethod('enableInputMethod', args);
-  }
-
-  @override
-  Future<void> showInputMethod() async {
-    Map<String, dynamic> args = <String, dynamic>{};
-    return await channel?.invokeMethod('showInputMethod', args);
-  }
-
-  @override
-  Future<void> hideInputMethod() async {
-    Map<String, dynamic> args = <String, dynamic>{};
-    return await channel?.invokeMethod('hideInputMethod', args);
+    return await channel
+        ?.invokeMethod('enableInputMethod', args)
+        .then((_) => channel?.invokeMethod('showInputMethod', args));
   }
 
   @override

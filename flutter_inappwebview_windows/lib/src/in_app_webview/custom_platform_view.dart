fa225bf8a5f330ea32c95fa644c3c433aa5bc8fd
GooRingX <gooringx@gmail.com>
2025-01-09 16:16:12 +0800

    fix TypeError: Null check operator used on a null value
    custom_platform_view.dart  _CustomPlatformViewState._reportWidgetPosition  476 flutter_inappwebview_windows

--- flutter_inappwebview_windows/lib/src/in_app_webview/custom_platform_view.dart
+++ flutter_inappwebview_windows/lib/src/in_app_webview/custom_platform_view.dart
@@ -473,6 +473,9 @@ class _CustomPlatformViewState extends State<CustomPlatformView>
     final box = _key.currentContext?.findRenderObject() as RenderBox?;
     if (box != null) {
       await _controller.ready;
+      if (!mounted) {
+        return;
+      }
       final position = box.localToGlobal(Offset.zero);
       unawaited(_controller._setPosition(
           position, widget.scaleFactor ?? window.devicePixelRatio));

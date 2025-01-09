f7e79d44d867ca1a2ca3a30f9994392ca2cd547e
Arthur Pires <arthur.pires@nubank.com.br>
2025-01-09 15:20:06 -0300

    Fixing error: method does not override any method from its superclass

--- flutter_inappwebview_ios/ios/Classes/InAppWebView/InAppWebView.swift
+++ flutter_inappwebview_ios/ios/Classes/InAppWebView/InAppWebView.swift
@@ -1429,7 +1429,7 @@ public class InAppWebView: WKWebView, UIScrollViewDelegate, WKUIDelegate,
         }
     }
     
-    public override func evaluateJavaScript(_ javaScriptString: String, completionHandler: ((Any?, Error?) -> Void)? = nil) {
+    open override func evaluateJavaScript(_ javaScriptString: String, completionHandler: (@MainActor (Any?, Error?) -> Void)? = nil) {
         if let applePayAPIEnabled = settings?.applePayAPIEnabled, applePayAPIEnabled {
             if let completionHandler = completionHandler {
                 completionHandler(nil, nil)
